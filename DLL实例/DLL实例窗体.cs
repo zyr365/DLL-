16d75f7d47f3f4c60a4ce3ee30f00d70eedb0a97
luogy <luogy@LAPTOP-EVLPURG4>
2020-03-07 13:40:22 +0800

    添加项目文件。

---
+++ DLL实例/DLL实例窗体.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DLL实例
+{
+    public partial class DLL实例窗体 : Form
+    {
+        public DLL实例窗体()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            operation operate = new operation();
+            int c;
+            
+             c = operate.getSum(199, 9);
+            
+            richTextBox1.Text = c.ToString();
+
+        }
+    }
+}
---
+++ DLL实例/operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL实例
+{
+    class operation
+    {
+        public int getSum(int a,int b)
+        {
+
+            return a + b;
+
+        }
+
+    }
+}
